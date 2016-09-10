2389a6156323968cf9760aea34c66f07b5108384
Rody <Rody10@mail.ru>
2016-09-10 18:36:45 +0600

    - Added scratch of BreadCrumbs

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,12 +3,15 @@ import {ROUTER_DIRECTIVES, Router} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
 import {routes} from "../../app.routes";
 import {User} from "../../model/user.model";
+import {BreadcrumbComponent} from "ng2-breadcrumb/src/breadcrumb/breadcrumb";
+import {BreadcrumbService} from "ng2-breadcrumb/src/breadcrumb/breadcrumbService";
 
 @Component({
   selector: 'navigation',
   styleUrls: [
     './navigation.scss'
   ],
+  directives: [BreadcrumbComponent],
   templateUrl: './navigation.template.html'
 })
 export class NavigationComponent {
@@ -17,7 +20,11 @@ export class NavigationComponent {
   private user: User;
 
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private breadcrumbService: BreadcrumbService) {
+    breadcrumbService.addFriendlyNameForRoute('/', 'Home');
+    breadcrumbService.addFriendlyNameForRoute('/courses-list', 'courses list');
+    // breadcrumbService.addFriendlyNameForRoute('/', 'Home Sweet Home');
     this.user = this.authService.getUser();
     this.authService.getObservableUser().subscribe((res:User) => {
       this.user = res;
--- src/app/index.ts
+++ src/app/index.ts
@@ -3,6 +3,7 @@ import {AuthService} from "./services/auth.service";
 import {LoggedInGuard} from "./guards/loggedIn.guard";
 import {CourseService} from "./services/course.service";
 import {UserService} from "./services/user.service";
+import {BreadcrumbService} from "ng2-breadcrumb/src/breadcrumb/breadcrumbService";
 export * from './app.component';
 export * from './app.routes';
 
@@ -11,6 +12,7 @@ export const APP_PROVIDERS = [
   AuthService,
   CourseService,
   UserService,
+  BreadcrumbService,
 
   LoggedInGuard
 ];
